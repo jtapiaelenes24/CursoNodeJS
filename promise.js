3a359af40124896a3cefbfaa6be45ed5a560196d
JT <jtapiaelenes24@gmail.com>
2024-08-13 21:22:29 +0200

    Añadido archivo promise.js

---
+++ promise.js
@@ -0,0 +1,20 @@
+const {readFile} = require('fs')
+
+const getText = (pathFile) => {
+    return new Promise((resolve, reject) => {
+        readFile(pathFile, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    }) 
+}
+
+getText('./data/second.txt')
+    .then((result) => console.log(result))
+    .then(() => getText('./data/first.txt'))
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
+
