030e94d9dd489840c255a63ab070f1318bbb1ee1
JT <jtapiaelenes24@gmail.com>
2024-08-22 22:39:54 +0200

    Añadidos esmodule.js y package.json

---
+++ esmodule.js
@@ -0,0 +1,6 @@
+import {add, substract, multiply, divide} from './math/index.js'
+
+console.log(add(10, 20))
+console.log(substract(10, 20))
+console.log(multiply(10, 20))
+console.log(divide(10, 20))
